556913a5dc0a058ff4fca945e5d5b97899afc00b
Ankit Patil <50051003+PatilAnkits@users.noreply.github.com>
2021-05-22 12:03:18 +0530

    Create data.sql

---
+++ sql-scripts/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE internData (
+name varchar(25),
+address  varchar(25),
+mobile_number varchar(15),
+pan_number  varchar(50)
+);
+
+INSERT INTO internData (name, address, mobile_number, pan_number) VALUES ('Ankit Patil', 'pune ', '8698502533', 'DRYPP3346E');
